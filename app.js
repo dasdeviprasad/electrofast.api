91cad501cfd6cba460184f880d40385ae47e6eb8
damoder reddy <damodar.prince@gmail.com>
2022-08-06 15:28:36 -0500

    Removed prefix for api

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cors());
 app.use(passport.initialize());
 
 // [SH] Use the API routes when path starts with /api
-app.use('/api', routesApi);
+app.use('', routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
