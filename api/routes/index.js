9f474da61835e3da2a7ffc5d8bff59c15f5c1aeb
damoder reddy <damodar.prince@gmail.com>
2022-08-06 17:20:44 -0500

    Changed login logic

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var userModel = require('../models/users');
 
 var ctrlAuth = require('../controllers/authentication');
 
 // authentication
 router.post('/register', ctrlAuth.register);
-router.post('/login', ctrlAuth.login);
+router.post('/login', (req, res) => {
+  console.log('Login Request', req.body);
+
+  if(!req.body.email || !req.body.password) {
+    return sendJSONresponse(res, 400, {
+      "message": "All fields required"
+    });
+  }
+
+  return sendJSONresponse(res, 400, {
+    "message": "All fields required"
+  });
+
+  console.log('authenticating);')
+  passport.authenticate('local', function(err, user, info){
+    var token;
+    console.log('Authenticated', err);
+
+    // If Passport throws/catches an error
+    if (err) {
+      res.status(404).json(err);
+      return;
+    }
+
+    // If a user is found
+    if(user){
+      token = user.generateJwt();
+      res.status(200);
+      res.json({
+        "token" : token
+      });
+    } else {
+      // If user is not found
+      res.status(401).json(info);
+    }
+  })(req, res);
+});
 
 router.get('/', (req, res) => {
   res.send('hello world!')
